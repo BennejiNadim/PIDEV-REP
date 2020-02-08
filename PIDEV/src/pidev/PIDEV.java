be88ade9fb714d111596b9ee365a11f88511d41e
mediniwael <44524958+mediniwael@users.noreply.github.com>
2020-02-08 20:17:00 +0100

    Add files via upload

--- PIDEV/src/pidev/PIDEV.java
+++ PIDEV/src/pidev/PIDEV.java
@@ -5,6 +5,9 @@
  */
 package pidev;
 
+import Entities.Facture;
+import Services.GestionFacture;
+
 /**
  *
  * @author ASUS
@@ -15,7 +18,25 @@ public class PIDEV {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        
+        //Facture f = new Facture(Facture.etat.not_payed, 8453, "nnn", "Employe1", 22, Facture.typef.salaire);
+        //Facture f2 = new Facture(Facture.etat.not_payed, 544, "nnn", "nnn", 22, Facture.typef.taxe);
+        Facture f3 = new Facture(Facture.etat.not_payed, 787, "Client1", "", 22, Facture.typef.vente_produit);
+        Facture f4 = new Facture(Facture.etat.not_payed, 222, "nnn", "nnn", 886, Facture.typef.achat_produit);
+        
+        f3.setPayed();
+        f3.setId(102);
+        GestionFacture gf = new GestionFacture();
+        
+        gf.modifierFacture(f3);
+        //gf.ajouterFacture(f);
+        //gf.ajouterFacture(f2);
+        //gf.ajouterFacture(f3);
+        //gf.ajouterFacture(f4);
+        gf.afficherFacture();
+        
+        */
     }
     
 }
