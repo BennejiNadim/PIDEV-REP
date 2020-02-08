0f194509e2813d83bee98d914b35eb4de129aa94
BennejiNadim <51533022+BennejiNadim@users.noreply.github.com>
2020-02-08 22:07:24 +0100

    Add files via upload

---
+++ PIDEV/src/Services/GestionMouvementProduit.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Entities.MouvementProduit;
+import Utils.Connexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ASUS
+ */
+public class GestionMouvementProduit {
+
+    private Connection con = Connexion.getInstance().getCnx();
+
+    /**
+     * ****************AJOUT****************
+     */
+    public void ajouterMouvement(MouvementProduit M) {
+        Statement st;
+        try {
+            st = con.createStatement();
+            String req = "insert into MouvementProduit values(" + M.getId_mouvement_Produit() + "," + M.getId_facture()
+                    + "," + M.getSource() + "," + M.getDestination() + "," + M.getQuantité() + "," + M.getDate() + ")";
+            st.executeUpdate(req);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionMouvementProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * ******************AFFICHAGE******************
+     */
+    public void afficherListeMouvement() {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("select * from MouvementProduit");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Mouvement [id_mouvement : " + rs.getInt(1) + " , id_facture :" + rs.getString(2) + " , source : " + rs.getString(3) + " , destination : " + rs.getString(4) + " , quantité: " + rs.getInt(5) + " , Date " + rs.getDate(6) + "]");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionMouvementProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * ***********SUPPRESSION********************
+     */
+    public void supprimerMouvement(int id) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("delete from mouvementProduit where id_mouvement_Produit = ?");
+            pt.setInt(1, id);
+            pt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionMouvementProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * *************MODIFICATION********************
+     */
+    public void modifierMouvement(MouvementProduit m) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("update MouvementProduit set source = ? , destination = ? , quantité = ? , date = ? where id_mouvement_Produit = ?");
+            pt.setString(1, m.getSource().toString());
+            pt.setString(2, m.getDestination().toString());
+            pt.setInt(3, m.getQuantité());
+            pt.setDate(4, m.getDate());
+            pt.setInt(5, m.getId_mouvement_Produit());
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionMouvementProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
--- PIDEV/src/Services/GestionProduit.java
+++ PIDEV/src/Services/GestionProduit.java
@@ -5,10 +5,133 @@
  */
 package Services;
 
+import Entities.Produit;
+import Entities.Produit;
+import Utils.Connexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ASUS
  */
 public class GestionProduit {
-    
+
+    private Connection con = Connexion.getInstance().getCnx();
+
+    /**
+     * **********AJOUT***********************
+     */
+    public void ajouterProduit(Produit P) {
+        Statement st;
+        try {
+            st = con.createStatement();
+            String req = "insert into Produit values('" + P.getRef_produit() + "','" + P.getNom_produit() + "','" + P.getMarque() + "','" + P.getCategorie() + "'," + P.getQuantité_stock() + " , " + P.getQuantité_magasin() + " , " + P.getPrix_vente() + " , " + P.getPrix_achat() + ")";
+            st.executeUpdate(req);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * ******AFFICHAGE********************
+     */
+    public void afficherProduit() {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("select * from Produit");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Produit [reference : " + rs.getInt(1) + " , nom produit :" + rs.getString(2) + " , marque : " + rs.getString(3) + " , categorie : " + rs.getString(4) + " , quantité stock : " + rs.getInt(5) + " , quantité magasin" + rs.getInt(6) + ", prix vente :" + rs.getInt(7) + " , prix achat :" + rs.getInt(8) + "]");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void afficherProduitParCategorie(String categorie) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("select * from Produit where categorie = '" + categorie + "'");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Produit [reference : " + rs.getInt(1) + " , nom produit :" + rs.getString(2) + " , marque : " + rs.getString(3) + " , categorie : " + rs.getString(4) + " , quantité stock : " + rs.getInt(5) + " , quantité magasin" + rs.getInt(6) + ", prix vente :" + rs.getInt(7) + " , prix achat :" + rs.getInt(8) + "]");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void afficherProduitParMarque(String Marque) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("select * from Produit where marque = '" + Marque + "'");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Produit [reference : " + rs.getInt(1) + " , nom produit :" + rs.getString(2) + " , marque : " + rs.getString(3) + " , categorie : " + rs.getString(4) + " , quantité stock : " + rs.getInt(5) + " , quantité magasin" + rs.getInt(6) + ", prix vente :" + rs.getInt(7) + " , prix achat :" + rs.getInt(8) + "]");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void afficherProduitParRef(int ref) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("select * from Produit where ref = " + ref);
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Produit [reference : " + rs.getInt(1) + " , nom produit :" + rs.getString(2) + " , marque : " + rs.getString(3) + " , categorie : " + rs.getString(4) + " , quantité stock : " + rs.getInt(5) + " , quantité magasin" + rs.getInt(6) + ", prix vente :" + rs.getInt(7) + " , prix achat :" + rs.getInt(8) + "]");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * ************SUPPRESSION**********************
+     */
+    public void supprimerProduit(int ref) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("delete from Produit where ref_produit = ?");
+            pt.setInt(1, ref);
+            pt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * ************MODIFICATION********************
+     */
+    public void modifierProduit(Produit p) {
+        PreparedStatement pt;
+        try {
+            pt = con.prepareStatement("update produit set nom_produit = ? , marque = ? , categorie = ? , quantité_stock = ? , quantité_magasin = ? , prix_vente = ? , prix_achat = ?  where ref_produit = ?");
+            pt.setString(1, p.getNom_produit());
+            pt.setString(2, p.getMarque());
+            pt.setString(3, p.getCategorie());
+            pt.setInt(4, p.getQuantité_stock());
+            pt.setInt(5, p.getQuantité_magasin());
+            pt.setFloat(6, p.getPrix_vente());
+            pt.setFloat(7, p.getPrix_achat());
+            pt.setInt(8, p.getRef_produit());
+            pt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionProduit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
